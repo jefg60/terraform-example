daf5a8e80384217afcaf926c74bba1b67c48203d
Jeff Hibberd <jeff@jeffhibberd.com>
2018-06-19 17:01:25 +0100

    variablized security group

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["82.15.223.149/32"]
   }
