d91ab7771812ddd115983da0ec06160c70ff03c9
Jeff Hibberd <jeff@jeffhibberd.com>
2018-06-19 17:46:00 +0100

    added listener

--- elb.tf
+++ elb.tf
@@ -2,3 +2,14 @@ resource "aws_elb" "example" {
   name = "terraform-asg-example"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
 }
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+}
