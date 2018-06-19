49ea869bd88fda4c708e2fb5f6cb8e0045570c02
Jeff Hibberd <jeff@jeffhibberd.com>
2018-06-19 17:49:08 +0100

    fixed dup resource, added healthcheck

--- elb.tf
+++ elb.tf
@@ -1,11 +1,14 @@
 resource "aws_elb" "example" {
   name = "terraform-asg-example"
+  security_groups = ["${aws_security_group.elb.id}"]
   availability_zones = ["${data.aws_availability_zones.all.names}"]
-}
-
-resource "aws_elb" "example" {
-  name = "terraform-asg-example"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
   listener {
     lb_port = 80
     lb_protocol = "http"
