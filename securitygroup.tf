3890a16cab14c94738a54d2cf0e996488cf42715
Jeff Hibberd <jeff@jeffhibberd.com>
2018-06-19 16:52:22 +0100

    security group

---
+++ securitygroup.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["82.15.223.149/32"]
+  }
+}
