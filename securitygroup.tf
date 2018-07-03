27f102eaa0e9e8449b53f0af043dc82e2e268c0f
Jeff Hibberd <jeff@jeffhibberd.com>
2018-07-03 13:53:47 +0100

    add ssh rule to sec grp for instances

--- securitygroup.tf
+++ securitygroup.tf
@@ -6,6 +6,12 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = ["82.15.223.149/32"]
+  }
 
   lifecycle {
     create_before_destroy = true
