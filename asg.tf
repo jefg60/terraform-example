3f73479abef4882dea13a0cd2c2f7094340ba9a2
Jeff Hibberd <jeff@jeffhibberd.com>
2018-07-03 15:09:42 +0100

    minimum 0 instances

--- asg.tf
+++ asg.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
 
-  min_size = 2
+  min_size = 0
   max_size = 10
 
   load_balancers = ["${aws_elb.example.name}"]
