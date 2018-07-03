86aae79cfb408df251346a6b042bae76fa218eca
Jeff Hibberd <jeff@jeffhibberd.com>
2018-07-03 14:44:24 +0100

    comment

--- elb.tf
+++ elb.tf
@@ -1,5 +1,6 @@
 resource "aws_elb" "example" {
   name = "terraform-asg-example"
+  #This only works in a VPC
   #security_groups = ["${aws_security_group.elb.id}"]
   availability_zones = ["${data.aws_availability_zones.all.names}"]
   health_check {
