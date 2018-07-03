324ee3bc86bfc296284edb039a586c411b110964
Jeff Hibberd <jeff@jeffhibberd.com>
2018-07-03 13:54:07 +0100

    moved launchconfig out of main

---
+++ launchconfig.tf
@@ -0,0 +1,17 @@
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-58d7e821"
+  instance_type = "t1.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+  key_name = "jwhaws"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
--- main.tf
+++ main.tf
@@ -2,19 +2,3 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id = "ami-58d7e821"
-  instance_type = "t1.micro"
-  security_groups = ["${aws_security_group.instance.id}"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-}
