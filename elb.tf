b4583421aa08f8ce30b23d72ec674d2184f39707
Jeff Hibberd <jeff@jeffhibberd.com>
2018-07-03 14:14:50 +0100

    change elb healthcheck intervals

--- elb.tf
+++ elb.tf
@@ -5,8 +5,8 @@ resource "aws_elb" "example" {
   health_check {
     healthy_threshold = 2
     unhealthy_threshold = 2
-    timeout = 3
-    interval = 30
+    timeout = 2
+    interval = 6
     target = "HTTP:${var.server_port}/"
   }
   listener {
